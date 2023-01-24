b6e7532c3787cc41710578db20a15b1b25649622
Seiyoung Park <seii0kor@gmail.com>
2023-01-24 21:53:17 +0900

    뭔가 다 날리고 다시 시작함

--- src/HelloWorld.tsx
+++ src/HelloWorld.tsx
@@ -1,65 +1,30 @@
-import { spring } from 'remotion';
-import {
-	AbsoluteFill,
-	interpolate,
-	Sequence,
-	useCurrentFrame,
-	useVideoConfig,
-} from 'remotion';
-import { Subtitle } from './HelloWorld/Subtitle';
-import { Title } from './HelloWorld/Title';
-import { Solid } from './HelloWorld/solid';
+import { AbsoluteFill } from 'remotion';
 
-export const HelloWorld: React.FC<{
-	titleText: string;
-	titleColor: string;
-}> = ({ titleText, titleColor }) => {
-	const frame = useCurrentFrame();
-	const { durationInFrames } = useVideoConfig();
-
-	// // Animate from 0 to 1 after 25 frames
-	// const solidTranslationProgress = spring({
-	// 	frame: frame - 25,
-	// 	fps,
-	// 	config: {
-	// 		damping: 100,
-	// 	},
-	// });
-
-	// // Move the logo up by 150 pixels once the transition starts
-	// const solidTranslation = interpolate(
-	// 	solidTranslationProgress,
-	// 	[0, 1],
-	// 	[0, -150]
-	// );
-
-	// Fade out the animation at the end
-	const opacity = interpolate(
-		frame,
-		[durationInFrames - 25, durationInFrames - 15],
-		[1, 0],
-		{
-			extrapolateLeft: 'clamp',
-			extrapolateRight: 'clamp',
-		}
-	);
-
-	// A <AbsoluteFill> is just a absolutely positioned <div>!
+export const HelloWorld = () => {
 	return (
-		<AbsoluteFill style={{ backgroundColor: 'white', }}>
-			<AbsoluteFill style={{ opacity }}>
-				{/* <AbsoluteFill style={{ transform: `translateY(${solidTranslation}px)` }}> */}
-				<Solid />
-				{/* </AbsoluteFill> */}
-				{/* Sequences can shift the time for its children! */}
-				<Sequence from={35}>
-					<Title titleText={titleText} titleColor={titleColor} />
-				</Sequence>
-				{/* The subtitle will only enter on the 75th frame. */}
-				<Sequence from={75}>
-					<Subtitle />
-				</Sequence>
-			</AbsoluteFill>
+		<AbsoluteFill style={{
+			backgroundColor: 'white',
+			flexDirection: 'column',
+			justifyContent: 'flex-end',
+			alignItems: 'flex-end',
+		}}>
+			<div style={{
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'center',
+			}}>
+				<div style={{
+					width: 400,
+					height: 400,
+					backgroundColor: '#2B28B8',
+				}} />
+				<h1 style={{
+					textAlign: 'center',
+					borderStyle: 'solid',
+				}}>
+					전수열
+				</h1>
+			</div>
 		</AbsoluteFill>
 	);
 };
