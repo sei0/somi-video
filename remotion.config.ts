986af4651ecb44ca16729bd1040ddf64300d42cd
Seiyoung Park <seii0kor@gmail.com>
2023-01-25 00:53:24 +0900

    HDR 영상이 나오도록 설정

--- remotion.config.ts
+++ remotion.config.ts
@@ -2,7 +2,8 @@
 // Each option also is available as a CLI flag: https://remotion.dev/docs/cli
 // ! The configuration file does only apply if you render via the CLI !
 
-import {Config} from 'remotion';
+import { Config } from 'remotion';
 
 Config.Rendering.setImageFormat('jpeg');
 Config.Output.setOverwriteOutput(true);
+Config.Puppeteer.setChromiumOpenGlRenderer('angle') // HDR 렌더링을 위해 필요 https://indentcorp.slack.com/archives/C04599WR6AH/p1671808018940539
