2099cc0d1138717b03fe8f48ecfa7ded4525b934
Seiyoung Park <seii0kor@gmail.com>
2023-02-07 11:11:04 +0900

    Add another template

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,5 +1,6 @@
 import { Composition } from 'remotion';
 import { HelloWorld } from './HelloWorld';
+import { template } from './template-1';
 
 import './styles/global.css'
 
@@ -11,18 +12,26 @@ export const RemotionRoot: React.FC = () => {
 			<Composition
 				// You can take the "id" to render a video:
 				// npx remotion render src/index.ts <id> out/video.mp4
-				id="HelloWorld"
+				id="test"
 				component={HelloWorld}
 				durationInFrames={150}
 				fps={30}
 				width={1080}
 				height={1920}
-				// You can override these props for each render:
-				// https://www.remotion.dev/docs/parametrized-rendering
-				defaultProps={{
-					titleText: 'Welcome to Somi-video',
-					titleColor: 'black',
-				}}
+			// You can override these props for each render:
+			// https://www.remotion.dev/docs/parametrized-rendering
+			// defaultProps={{
+			// 	titleText: 'Welcome to Somi-video',
+			// 	titleColor: 'black',
+			// }}
+			/>
+			<Composition
+				id="template-1"
+				component={template}
+				durationInFrames={150}
+				fps={30}
+				width={1080}
+				height={1920}
 			/>
 		</>
 	);
---
+++ src/template-1.tsx
@@ -0,0 +1,96 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import styled from 'styled-components'
+import { AbsoluteFill, spring, useCurrentFrame, useVideoConfig } from 'remotion';
+
+import { staticFile, Video } from "remotion";
+
+export const template = () => {
+	const currentFrame = useCurrentFrame()
+	const { fps } = useVideoConfig();
+
+	const textOpacity = (currentFrame - (0.5 * fps)) / (0.5 * fps)
+
+	const textTranslateY = spring({
+		frame: currentFrame - (0.5 * fps),
+		fps,
+		from: -30,
+		to: 0,
+		config: {
+			mass: 0.4
+		}
+	})
+	const textTransform = `translateY(${textTranslateY}px)`
+
+	const captionOpacity = (currentFrame - (1.5 * fps)) / (0.5 * fps)
+
+	const captionTranslateY = spring({
+		frame: currentFrame - (1.5 * fps),
+		fps,
+		from: -10,
+		to: 0,
+		config: {
+			mass: 0.4
+		}
+	})
+	const captionTransform = `translateY(${captionTranslateY}px)`
+
+
+	return (
+		<AbsoluteFill style={{
+			backgroundColor: 'white',
+			flexDirection: 'column',
+			justifyContent: 'flex-start',
+			alignItems: 'center',
+		}}>
+			<Container>
+				<VideoBox>
+					<ContentVideo src={staticFile("IMG_4828.mov")} />
+				</VideoBox>
+				<Text style={{ opacity: textOpacity, transform: textTransform }}>Hello this is test</Text>
+			</Container>
+			<Caption style={{ opacity: captionOpacity, transform: captionTransform }} >Made by SEI</Caption>
+		</AbsoluteFill>
+	);
+};
+
+const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	width: 100%;
+	/* Inside auto layout */
+	flex: none;
+	order: 0;
+	align-self: stretch;
+	flex-grow: 1;
+`
+const VideoBox = styled.div`
+	width: 100%;
+	aspect-ratio: 1;
+	overflow: hidden;
+`
+const ContentVideo = styled(Video)`
+	width: 100%;
+	height: 100%;
+	object-fit: cover;
+`
+
+const Text = styled.h1`
+	display: flex;
+	text-align: center;
+	font-family: poppins;
+	font-weight: 500;
+	font-size: 60px;
+	line-height: 86px;
+	color: #17171A;
+	padding: 200px 100px 0px;
+`
+const Caption = styled.h2`
+	display: flex;	
+	font-family: 'Poppins';
+	font-weight: 500;
+	font-size: 40px;
+	line-height: 60px;
+	color: #76767C;
+	padding: 0px 0px 80px;
+`
